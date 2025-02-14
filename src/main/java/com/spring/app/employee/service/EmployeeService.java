c0170797addb9518ce5bbe5636074ba332df476c
leejihye02 <bamu6651@gmail.com>
2025-02-14 16:05:09 +0900

    fix : 맵핑 적용
    .

--- src/main/java/com/spring/app/employee/service/EmployeeService.java
+++ src/main/java/com/spring/app/employee/service/EmployeeService.java
@@ -1,11 +1,22 @@
 package com.spring.app.employee.service;
 
-import java.util.List;
+
 import java.util.Map;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public interface EmployeeService {
 
-	List<Map<String, String>> test();
+//	List<Map<String, String>> test();
+
+	// 로그인 처리하기
+	int login(HttpServletRequest request, Map<String, String> paraMap, HttpServletResponse response, ModelAndView mav);
+
+
+
 
 
 }
