78c72ce309640567742dcad25d1e75c3a91344a9
isleesangwoo <giyf12081208@gmail.com>
2025-02-14 15:45:48 +0900

    feat : BoardDAO.JSP @mapper 추가
    BoardDAO_imple 삭제 BoardDAO.jsp에 @mapper 추가

--- src/main/java/com/spring/app/board/model/BoardDAO.java
+++ src/main/java/com/spring/app/board/model/BoardDAO.java
@@ -3,6 +3,9 @@ package com.spring.app.board.model;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface BoardDAO {
 	
 	// 게시판 생성하기
