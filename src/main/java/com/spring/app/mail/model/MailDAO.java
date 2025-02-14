55b89b385ef4c2a7701e232777616305cb482151
ehdgns6402 <ehdgns6402@naver.com>
2025-02-14 15:48:24 +0900

    fix : 메일 페이지 추가, DAO 형식변경
    DAO_imple 삭제 후 mapper 사용

--- src/main/java/com/spring/app/mail/model/MailDAO.java
+++ src/main/java/com/spring/app/mail/model/MailDAO.java
@@ -3,6 +3,11 @@ package com.spring.app.mail.model;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper //@Mapper 어노테이션을 붙여서 DAO 역할의 Mapper 인터페이스 파일을 만든다. 
+		//EmpDAO 인터페이스를 구현한 DAO 클래스를 생성하면 오류가 뜨므로 절대로 DAO 클래스를 생성하면 안된다.!!! 
+		//@Mapper 어노테이션을 사용하면 빈으로 등록되며 Service단에서 @Autowired 하여 사용할 수 있게 된다.
 public interface MailDAO {
 
 	List<Map<String, String>> test();
